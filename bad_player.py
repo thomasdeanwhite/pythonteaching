267025d2c2f2510035a0bc6d86db040800c76475
Thomas White <tdwhite1@sheffield.ac.uk>
2019-03-07 11:35:06 +0000

    Player that goes column by column

--- bad_player.py
+++ bad_player.py
@@ -1,17 +1,23 @@
 from player import Player
 import random
 
-class RandomPlayer(Player):
+class BadPlayer(Player):
 
     def __init__(self, player_num):
         self.player_num = player_num
 
     def make_move(self, connect_four):
-        move = random.randint(0, connect_four.grid_size[1]-1)
+        move = 0
 
         while not connect_four.is_valid_move(move):
-            move = random.randint(0, connect_four.grid_size[1]-1)
+            move += 1
+
+        board = connect_four.copy()
+        board.make_move(2)
+
+        if board.max_in_row(self.player_num+1) > 1:
+            return 2
         return move
 
     def get_name(self):
-        return "Random Player"
+        return "Bad Player"
